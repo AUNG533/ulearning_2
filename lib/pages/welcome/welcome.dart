6b93fa5f9c10d9f30049fa7fe71d03355154dbeb
hongs <aungkyawlinmon@gmail.com>
2024-06-04 10:18:26 +0700

    welcome part 5 animation and navigator

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -12,71 +12,71 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Scaffold(
-        body: BlocBuilder<WelcomeBloc, WelcomeState> (
-          builder: (context, state) {
-            return Container(
-              margin: EdgeInsets.only(top: 34.h),
-              width: 375.w,
-              child: Stack(
-                alignment: Alignment.topCenter,
-                children: [
-                  PageView(
-                    onPageChanged: (index) {
-                      state.page = index;
-                      BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
-                      print("index value : $index");
-                    },
-                    children: [
-                      _page(
-                          1,
-                          context,
-                          "next",
-                          "First see Learning",
-                          "Forget about a for of paper all knowledge in one learning",
-                          "assets/images/reading.png"),
-                      _page(
-                          2,
-                          context,
-                          "next",
-                          "Connect With Everyone",
-                          "Always keep in touch with your tutor & friend. Let's get connected",
-                          "assets/images/boy.png"),
-                      _page(
-                          3,
-                          context,
-                          "get started",
-                          "Always Fascinated Learning",
-                          "Anywhere, anytime. The time is at our discretion so study whenever you want",
-                          "assets/images/man.png"),
-                    ],
-                  ),
-                  Positioned(
-                    bottom: 100,
-                    child: DotsIndicator(
-                      position: state.page,
-                      dotsCount: 3,
-                      decorator: DotsDecorator(
-                        color: Colors.grey,
-                        activeColor: Colors.blue,
-                        size: const Size.square(8.0),
-                        activeSize: const Size(18.0, 8.0),
-                        activeShape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
+      child: Scaffold(body: BlocBuilder<WelcomeBloc, WelcomeState>(
+        builder: (context, state) {
+          return Container(
+            margin: EdgeInsets.only(top: 34.h),
+            width: 375.w,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                PageView(
+                  controller: pageController,
+                  onPageChanged: (index) {
+                    state.page = index;
+                    BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
+                  },
+                  children: [
+                    _page(
+                        1,
+                        context,
+                        "next",
+                        "First see Learning",
+                        "Forget about a for of paper all knowledge in one learning",
+                        "assets/images/reading.png"),
+                    _page(
+                        2,
+                        context,
+                        "next",
+                        "Connect With Everyone",
+                        "Always keep in touch with your tutor & friend. Let's get connected",
+                        "assets/images/boy.png"),
+                    _page(
+                        3,
+                        context,
+                        "get started",
+                        "Always Fascinated Learning",
+                        "Anywhere, anytime. The time is at our discretion so study whenever you want",
+                        "assets/images/man.png"),
+                  ],
+                ),
+                Positioned(
+                  bottom: 100,
+                  child: DotsIndicator(
+                    position: state.page,
+                    dotsCount: 3,
+                    decorator: DotsDecorator(
+                      color: Colors.grey,
+                      activeColor: Colors.blue,
+                      size: const Size.square(8.0),
+                      activeSize: const Size(18.0, 8.0),
+                      activeShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
                       ),
                     ),
                   ),
-                ],
-              ),
-            );
-          },
-        )
-      ),
+                ),
+              ],
+            ),
+          );
+        },
+      )),
     );
   }
 
@@ -111,27 +111,41 @@ class _WelcomeState extends State<Welcome> {
             ),
           ),
         ),
-        Container(
-          margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
-          width: 325.w,
-          height: 50.w,
-          decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(15.w)),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 2,
-                    offset: const Offset(0, 1))
-              ]),
-          child: Center(
-            child: Text(
-              buttonName,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16.sp,
-                fontWeight: FontWeight.normal,
+        GestureDetector(
+          onTap: () {
+            //within 0-2 index
+            if (index < 3) {
+              //animation
+              pageController.animateToPage(index,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.decelerate);
+            } else {
+              //jump to a new page
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ))
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+            width: 325.w,
+            height: 50.w,
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                      offset: const Offset(0, 1))
+                ]),
+            child: Center(
+              child: Text(
+                buttonName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.normal,
+                ),
               ),
             ),
           ),
