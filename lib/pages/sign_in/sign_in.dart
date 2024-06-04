5c8cd5046fad9f5dc5ded5dfa52f2229cea3657e
hongs <aungkyawlinmon@gmail.com>
2024-06-04 19:40:48 +0700

    Login Ui and TextField

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_2/pages/sign_in/widgets/sign_in_widget.dart';
 
 class SignIn extends StatefulWidget {
@@ -18,12 +19,30 @@ class _SignInState extends State<SignIn> {
           backgroundColor: Colors.white,
           appBar: buildAppBar(),
           body: SingleChildScrollView(
-              child: Column(
-                children: [
-                  buildThirdPartyLogin(context),
-                  reusableText("Or use your email account to login"),
-                ],
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildThirdPartyLogin(context),
+                Center(
+                    child: reusableText("Or use your email account to login")),
+                Container(
+                  margin: EdgeInsets.only(top: 66.h),
+                  padding: EdgeInsets.only(left: 25.w, right: 25.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      reusableText("Email"),
+                      SizedBox(height: 5.h),
+                      buildTextField(
+                          "Enter your email address", "email", "user"),
+                      reusableText("Password"),
+                      SizedBox(height: 5.h),
+                      buildTextField("Enter your password", "password", "lock"),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/sign_in/widgets/sign_in_widget.dart
+++ lib/pages/sign_in/widgets/sign_in_widget.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -57,6 +58,70 @@ Widget reusableText(String text) {
         fontWeight: FontWeight.normal,
         fontSize: 14.sp,
       ),
+    ),
+  );
+}
+
+Widget buildTextField(String hintText, String textType, String iconName) {
+  return Container(
+    margin: EdgeInsets.only(bottom: 20.h),
+    width: 325.w,
+    height: 50.h,
+    decoration: BoxDecoration(
+        color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+      border: Border.all(color: Colors.black)
+
+    ),
+    child: Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 17.w),
+          width: 16.w,
+          height: 16.h,
+          child: Image.asset("assets/icons/$iconName.png"),
+        ),
+        SizedBox(
+
+          width: 270.w,
+          height: 50.h,
+          child: TextField(
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              hintText: hintText,
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                )
+              ),
+              enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
+                  ),
+              ),
+              disabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
+                  ),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
+                  )
+              ),
+              hintStyle: TextStyle(color: Colors.grey.withOpacity(0.5))
+            ),
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: "Avenir",
+              fontSize: 16.sp,
+              fontWeight: FontWeight.normal,
+            ),
+            autocorrect: false,
+            obscureText: textType == "password" ? true : false,
+          ),
+        )
+      ],
     ),
   );
 }
