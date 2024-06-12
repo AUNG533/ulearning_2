558b661efd67f23880faa5acf54d95a1819e21e3
hongs <aungkyawlinmon@gmail.com>
2024-06-12 12:20:03 +0700

    logout button

--- lib/pages/profile/settings/settings_page.dart
+++ lib/pages/profile/settings/settings_page.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ulearning_2/common/routes/names.dart';
+import 'package:ulearning_2/pages/application/bloc/app_blocs.dart';
+import 'package:ulearning_2/pages/profile/settings/widgets/settings_widgets.dart';
 
+import '../../../common/values/constant.dart';
+import '../../../global.dart';
 import 'bloc/settings_blocs.dart';
 import 'bloc/settings_states.dart';
 
@@ -12,18 +18,26 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  void removeUserData() {
+    context.read<AppBlocs>().add(const TriggerAppEvent(0));
+    Global.storageService.remove(AppConstants.STORAGE_USER_TOKEN_KEY);
+    Navigator.of(context)
+        .pushNamedAndRemoveUntil(AppRoutes.SIGN_IN, (route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(),
       body: SingleChildScrollView(
         child: BlocBuilder<SettingsBlocs, SettingsStates>(
           builder: (context, state) {
-            return Column(
-              children: [
-                Container(
-                  child: Text("Settings"),
-                )
-              ],
+            return Container(
+              child: Column(
+                children: [
+                    logoutButton(context, removeUserData)
+                ],
+              ),
             );
           },
         ),
